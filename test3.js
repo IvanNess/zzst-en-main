352c7a6c86813cab36b178f623f813fd8eaec0e7
unknown <chai@bk.ru>
2019-02-05 23:31:23 +0300

    v.0

--- test3.js
+++ test3.js
@@ -1,25 +1,26 @@
 
 $(document).ready(function(){
+/*    EXPLAINER VIDEOS -  ADVERTISING VIDEOS - TUTORIALS - VIDEO CONTENT */
 
-setTimeout(function(){$('#bannerText4').text('поясняющие\u00A0видео')}, 0);
+setTimeout(function(){$('#bannerText4').text('explainer\u00A0videos')}, 0);
 
-setTimeout(function(){$('#bannerText4').text('рекламные\u00A0видео')}, 2000);
+setTimeout(function(){$('#bannerText4').text('advertising\u00A0videos')}, 2000);
 
-setTimeout(function(){$('#bannerText4').text('обучающие\u00A0видео')}, 4000);
+setTimeout(function(){$('#bannerText4').text('tutorials')}, 4000);
 
-setTimeout(function(){$('#bannerText4').text('видеоконтент')}, 6000);
+setTimeout(function(){$('#bannerText4').text('video\u00A0content')}, 6000);
 
-setTimeout(function(){$('#bannerText4').text('продвижение\u00A0видео')}, 8000);
+/*setTimeout(function(){$('#bannerText4').text('продвижение\u00A0видео')}, 8000);*/
 
 
-setTimeout(function(){setInterval(function(){$('#bannerText4').text('поясняющие\u00A0видео')}, 10000);}, 0);
+setTimeout(function(){setInterval(function(){$('#bannerText4').text('explainer\u00A0videos')}, 10000);}, 0);
 
-setTimeout(function(){setInterval(function(){$('#bannerText4').text('рекламные\u00A0видео')}, 10000);}, 2000);
+setTimeout(function(){setInterval(function(){$('#bannerText4').text('advertising\u00A0videos')}, 10000);}, 2000);
 
-setTimeout(function(){setInterval(function(){$('#bannerText4').text('обучающие\u00A0видео')}, 10000);}, 4000);
+setTimeout(function(){setInterval(function(){$('#bannerText4').text('tutorials')}, 10000);}, 4000);
 
-setTimeout(function(){setInterval(function(){$('#bannerText4').text('видеоконтент')}, 10000);}, 6000);
+setTimeout(function(){setInterval(function(){$('#bannerText4').text('video\u00A0content')}, 10000);}, 6000);
 
-setTimeout(function(){setInterval(function(){$('#bannerText4').text('продвижение\u00A0видео')}, 10000);}, 8000);
+/*setTimeout(function(){setInterval(function(){$('#bannerText4').text('продвижение\u00A0видео')}, 10000);}, 8000);*/
 });
 
